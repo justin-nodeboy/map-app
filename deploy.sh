67808d0746ed4b8fadabe8e42e55ef0674537335
Justin Howard <justinhoward@Justins-MacBook-Pro-3.local>
2023-02-17 10:19:35 +0000

    What

--- deploy.sh
+++ deploy.sh
@@ -23,7 +23,7 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# if you are deploying to https://<USERNAME>.github.io/<REPO>≈
 git push -f git@github.com:justin-nodeboy/map-app.git main:gh-pages
 
 cd -
